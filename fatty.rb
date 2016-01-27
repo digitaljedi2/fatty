347b6fddbc950444662623c41d12ba2999e0cf3f
Jonathan Poole <jpoole@digitaljedi.ca>
2016-01-26 19:59:15 -0500

    updated fatty.rb

--- fatty.rb
+++ fatty.rb
@@ -5,11 +5,16 @@
 
 puts "Welcome to Fatty"
 puts "Enter Weight (kg):"
-w = gets
+w = gets.to_i
 puts "Thanks, Height? (meters):"
-h = gets
+h = gets.to_i
 
 puts "You entered weight: #{w} and height of #{h}"
 
-bmi = w.to_i/h.to_i ** 2
+bmi = w/h ** 2
 puts "BMI: #{bmi}"
+puts "Calculating BMI"
+puts "#{bmi} is Underweight" if bmi < 18.5
+puts "#{bmi} is Normal" if bmi > 18.5 && bmi < 24.9
+puts "#{bmi} is overweight" if bmi > 25 && bmi < 29.9
+puts "#{bmi} is obese" if bmi > 30
